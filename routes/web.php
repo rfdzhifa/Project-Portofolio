88347cd0138f97f82f5447cb22b093bffdbb6d4c
rfdzhifa <rfdnazhifah@gmail.com>
2022-01-19 08:43:22 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function() {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function() {
     return view('about', [
+        "title" => "About",
         "nama" => "Rifdah Inas Nazhifah",
         "email" => "3103120195@student.smktelkom-pwt.sch.id",
         "gambar" => "nature.jpg"
@@ -25,5 +28,7 @@ Route::get('/about', function() {
 });
 
 Route::get('/gallery', function() {
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
