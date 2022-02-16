d85218c4c2adc4726dea275ff5ccf98a22ffc1e7
rfdzhifa <rfdnazhifah@gmail.com>
2022-02-16 08:47:13 +0700

    modul 5

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,20 +5,28 @@
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <a class="nav-link {{ $title === 'Home' ? 'active' : ''}}" aria-current="page" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link {{ $title === 'About' ? 'active' : ''}}" href="/about">About</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link {{ $title === 'Gallery' ? 'active' : ''}}" href="/gallery">Gallery</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link {{ $title === 'Contacts' ? 'active' : ''}}" href="/contacts">Contact Us</a>
-            </li>
-        </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link {{ $title === 'Home' ? 'active' : ''}}" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link {{ $title === 'About' ? 'active' : ''}}" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link {{ $title === 'Gallery' ? 'active' : ''}}" href="/gallery">Gallery</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link {{ $title === 'Contacts' ? 'active' : ''}}" href="/contacts">Contact Us</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route ('login')}}" >Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route ('register')}}">Register</a>
+                </li>
+            </ul>
         </div>
     </div>
-    </nav>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use App\Http\Controllers\ContactController;
 |
 */
 Route::get('/', function() {
-    return view('home', [
-        "title" => "Home"
+    return view('index', [
+        "title" => "Beranda"
     ]);
 });
 
@@ -35,3 +35,10 @@ Route::get('/gallery', function() {
 });
 
 Route::resource('/contacts', ContactController::class);
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
